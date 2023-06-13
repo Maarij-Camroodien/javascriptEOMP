6e36401ff76ec0e80f63035189be16898b046a05
Maarij-Camroodien <maarijcamroodien18@gmail.com>
2023-06-13 16:44:12 +0200

    adminjs

--- script/admin.js
+++ script/admin.js
@@ -4,82 +4,13 @@
 // let deleteButtons;
 
 let manga = JSON.parse(localStorage.getItem('manga'))
-JSON.parse(localStorage.getItem('manga')) [
-    {
-        id: 1,
-        name: "Jujutsu Kaisen Vol.1",
-        image: "https://i.postimg.cc/1tYFmYHT/jujutsu-kaisen-manga1.jpg",
-        detail: "Volume 1 of Jujutsu Kaisen",
-        price: "R250",
-    },
-    {
-        id: 2,
-        name: "Jujutsu Kaisen Vol.4",
-        image: "https://i.postimg.cc/FKPHyX3W/jujutsu-kaisen-manga2.png",
-        detail: "Volume 4 of Jujutsu Kaisen",
-        price: "R350",
-    },
-    {
-        id: 3,
-        name: "Chainsaw Man Vol.1",
-        image: "https://i.postimg.cc/j2mFGbFS/chainsaw-man-vol-1.jpg",
-        detail: "Volume 1 of Chainsaw Man",
-        price: "R300",
-    },
-    {
-        id: 4,
-        name: "Solo Leveling Vol.1",
-        image: "https://i.postimg.cc/XY661fF6/solo-leveling-vol-1-comic.jpg",
-        detail: "Volume 1 of Solo Leveling",
-        price: "R325",
-    },
-    {
-        id: 5,
-        name: "Attack on Titan Vol.10",
-        image: "https://i.postimg.cc/T1TdpBj0/attack-on-titan-vol-10.jpg",
-        detail: "Volume 10 of Attack on Titan",
-        price: "R250",
-    },
-    {
-        id: 6,
-        name: "Jujutsu Kaisen Vol.1",
-        image: "https://i.postimg.cc/1tYFmYHT/jujutsu-kaisen-manga1.jpg",
-        detail: "Volume 1 of Jujutsu Kaisen",
-        price: "R250",
-    },
-    {
-        id: 7,
-        name: "Jujutsu Kaisen Vol.1",
-        image: "https://i.postimg.cc/1tYFmYHT/jujutsu-kaisen-manga1.jpg",
-        detail: "Volume 1 of Jujutsu Kaisen",
-        price: "R250",
-    },
-    {
-        id: 8,
-        name: "Jujutsu Kaisen Vol.1",
-        image: "https://i.postimg.cc/1tYFmYHT/jujutsu-kaisen-manga1.jpg",
-        detail: "Volume 1 of Jujutsu Kaisen",
-        price: "R250",
-    },
-    {
-        id: 9,
-        name: "Jujutsu Kaisen Vol.1",
-        image: "https://i.postimg.cc/1tYFmYHT/jujutsu-kaisen-manga1.jpg",
-        detail: "Volume 1 of Jujutsu Kaisen",
-        price: "R250",
-    },
-    {
-        id: 10,
-        name: "Jujutsu Kaisen Vol.1",
-        image: "https://i.postimg.cc/1tYFmYHT/jujutsu-kaisen-manga1.jpg",
-        detail: "Volume 1 of Jujutsu Kaisen",
-        price: "R250",
-    }
-]
+let saveBtn = document.querySelector('#savebtn')
+// let editbtn = document.querySelector('#edit-changes')
 
-let display = document.getElementById('tablecontent')
+let display = document.querySelector('.tablecontent')
+let id = manga[manga.length-1] ? manga[manga.length-1].id + 1 : 1
 // clear array
-function loadData(manga) {
+function loadData() {
     // clear items
     display.innerHTML = ""
     // displaying on html
@@ -91,14 +22,47 @@ function loadData(manga) {
             <td><img src="${item.image}" style="width:5rem;"></img></td>
             <td>${item.detail}</td>
             <td>${item.price}</td>
-            <td><button onclick="editItem(${item.id})" data-bs-toggle="modal" data-bs-target="#Modal1" style=" border: none; background-color: white;"><i class="fa-solid fa-pen"></i></button></td>
-            <td><button style=" border: none; background-color: white;" onclick="delItem(${item.id})"><i class="fa-solid fa-trash-can"></button></i></td>
+            <td>
+            <!-- Button trigger modal -->
+            <button style=" border: none;" id="editbtn" onclick="editbtn(${item.id})" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <i class="bi bi-pencil"></i>
+            </button>
+
+           <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+           <div class="modal-dialog">
+           <div class="modal-content">
+           <div class="modal-header">
+           <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+           <div class="modal-body">
+           <h6 class="name">Name</h6>
+        <input id="name1" placeholder="Enter product name..." type="text" class="w-100">
+        <h6 class="imageurl">ImageUrl</h6>
+        <input id="image1" placeholder="Enter product image..." type="text" class="w-100">
+        <h6 class="details">Details</h6>
+        <input id="detail1" placeholder="Enter product details..." type="text" class="w-100">
+        <h6 class="price">Price</h6>
+        <input id="price1" placeholder="Enter product price..." type="text" class="w-100">
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" id="edit-changes">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+            <td><button style=" border: none; background-color: white;" onclick="deletebtn(${item.id})"><i class="bi bi-trash-fill"></i></button></td>
         </tr>
         `
     })
 }
+
+loadData()
+
 // delete function
-function delItem(id) {
+function deletebtn(id) {
     manga = manga.filter(item => {
         return item.id !== id
     })
@@ -108,75 +72,113 @@ function delItem(id) {
     loadData(manga)
 }
 // edit function
-function editItem(id) {
-    document.getElementById("name").value = manga[id].name;
-    document.getElementById("image").value = manga[id].image;
-    document.getElementById("detail").value = manga[id].detail;
-    document.getElementById("price").value = manga[id].price;
-}
-function editItem(){
-editButtons = [...document.querySelectorAll('.edit-btn')];
-editButtons.forEach((item)=>{
-    item.addEventListener('click', manga)
-})
-}
-function editmangaItem(){
-    let newName = prompt('Enter new name:');
-    let index = editButtons.indexOf(event.target);
-    manga[index].name = newName;
-    localStorage.setItem('manga', JSON.stringify(manga));
-    renderList();
-}
-    editItem();
-// sort type dropdown
-function GenreSort(e) {
-    const type = document;
-    if (type === "all") {
-        return loadData(manga)
-    }
+editbtn.addEventListener('click', editproducts)
+
+// function editproducts(item) {
+//     this.id = item.id;
+//     this.name = document.querySelector("#name1").value;
+//     this.image = document.querySelector("#image1").value;
+//     this.detail = document.querySelector("#detail1").value;
+//     this.price = document.querySelector("#price1").value;
+
+//     let itemsIndex = manga.findIndex((item)=>{
+//         return item.id === item.id;
+//     })
+
+//     products[itemsIndex] = {
+//     id: this.id,
+//     name: this.name, 
+//     image: this.image,
+//     detail: this.detail, 
+//     price: this.price
+// };
+//     localStorage.setItem('manga', JSON.stringify(manga));
+//     loadData();
+// }
+
+
+// function editbtn(id) {
+//     document.querySelector("#name1").value = manga[id].name;
+//     document.querySelector("#image1").value = manga[id].image;
+//     document.querySelector("#detail1").value = manga[id].detail;
+//     document.querySelector("#price1").value = manga[id].price;
+// }
+// function editbtn(){
+// editButtons = [...document.querySelectorAll('#edit-changes')];
+// editButtons.forEach((item)=>{
+//     item.addEventListener('click', manga)
+// }) 
+// }
+// function editmangaItem(event){
+//     let newName = prompt('Enter new name:');
+//     let index = editButtons.indexOf(event.target);
+//     manga[index].name = newName;
+//     localStorage.setItem('manga', JSON.stringify(manga));
+//     renderList();
+// }
+    // editbtn();
+// // sort type dropdown
+// function GenreSort(e) {
+//     const type = document;
+//     if (type === "all") {
+//         return loadData(manga)
+//     }
     // console.log(type);
     // const filtered = Music.filter((item) => item.genre == type);
     // console.log(filtered);
     // return loadData(filtered);
-}
-// textSearch
-const textSearch = (e) => {
-    const text = e.target.value;
-    const filteredItems = manga.filter((item) =>
-        item.name.toLowerCase().includes(text.toLowerCase())
-    );
-    loadData(filteredItems);
-};
-// sort price dropdown
-function priceSort(e) {
-    const type = e.target.value;
-    if (type === "all") {
-        return loadData(manga)
-    }
-    console.log(typeof type);
-    const filtered = manga.filter((item) => item.price == type);
-    return loadData(filtered);
-}
-function genreSort(e) {
-  const type = e.target.value;
-  if (type === type.innerHTML) {
-      return loadData(manga)
-  }
-  console.log(type);
-  const filtered = manga.filter((item) => item.genre == type);
-  return loadData(filtered);
-}
-function updateBtn(id) {
-    let name = document.getElementById("name").value;
-    let image = document.getElementById("image").value;
-    let manga = document.getElementById("manga").value;
-    let price = document.getElementById("price").value;
+// }
+// // textSearch
+// const textSearch = (e) => {
+//     const text = e.target.value;
+//     const filteredItems = manga.filter((item) =>
+//         item.name.toLowerCase().includes(text.toLowerCase())
+//     );
+//     loadData(filteredItems);
+// };
+// // sort price dropdown
+// function priceSort(e) {
+//     const type = e.target.value;
+//     if (type === "all") {
+//         return loadData(manga)
+//     }
+//     console.log(typeof type);
+//     const filtered = manga.filter((item) => item.price == type);
+//     return loadData(filtered);
+// }
+// function genreSort(e) {
+//   const type = e.target.value;
+//   if (type === type.innerHTML) {
+//       return loadData(manga)
+//   }
+//   console.log(type);
+//   const filtered = manga.filter((item) => item.genre == type);
+//   return loadData(filtered);
+// }
+
+saveBtn.addEventListener('click', add)
+
+function add() {
+    let name = document.querySelector("#name").value;
+    let image = document.querySelector("#image").value;
+    let detail = document.querySelector("#detail").value;
+    let price = document.querySelector("#price").value;
+
+    manga.push({
+        id: id,
+        name: name,
+        image: image,
+        detail: detail,
+        price: price
+    })
+    id++
     // update localStorage
     localStorage.setItem('manga', JSON.stringify(manga))
     // Show items
-    loadData(manga)
+    loadData()
+    // console.log(manga);
 }
-loadData(manga)
+// loadData()
 
 
 
